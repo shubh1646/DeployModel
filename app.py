6003dd1b18c6de85a51d749bda77e12ef71d0c88
shubh1646 <sharma.shubh@northeastern.edu>
2020-04-23 03:49:36 -0400

    model pkl

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 from sklearn.externals import joblib 
 app = Flask(__name__)
 
@@ -6,16 +6,22 @@ model = joblib.load("model.pkl")
 
 @app.route("/")
 def hello():
-   return render_template("index.html")
+   return "hello world"
 
 
-@app.route("/",methods=['POST'])
+@app.route("/a",methods=['POST'])
 def marks():
     if request.method == 'POST':
-        hours = float(request.form["hours"])
+        data = request.json
+        print(data["time"])
+        hours  = float(data["time"])
+       # hours = float(request.form["hours"])
         marks = model.predict([[hours]])[0][0]
         fmarks  = str(round(marks,1))
-    return render_template("index.html",your_marks = fmarks)
+        output = {'marks': fmarks}
+        
+    return output
+    #return render_template("index.html",your_marks = fmarks)
 
 if __name__ == '__main__':
     app.run(debug= True)
